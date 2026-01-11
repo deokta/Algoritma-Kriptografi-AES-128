c1ed4dcd8f3125af7d0f64dc026c368e3b2b0ba7
Rizki Okta Triadi <deokta390@gmail.com>
2026-01-11 22:04:02 +0700

    Termination

---
+++ Termination.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Termination
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = auth()?->user();
+        
+        if(!empty($user)){
+            return $next($request);
+        }
+
+        return abort(403);
+    }
+}
